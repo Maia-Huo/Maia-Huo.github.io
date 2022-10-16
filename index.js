72914b6198dcf34bd224b2f36a9200bfa6a68a03
Maia-Huo <93067331+Maia-Huo@users.noreply.github.com>
2022-10-16 22:25:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+////对date的实操
+
+// const now = new Date();
+// const win95Launch = new Date(1995,7,24);
+
+// console.log(now);
+// console.log(win95Launch);
+
+// const demoDate = new Date();
+// demoDate.setMonth(0);
+// console.log(demoDate);
+
+// console.log(`Day of week: ${demoDate.getDay()}`);
+// console.log(`Date: ${demoDate.getDate()}`);
+
+///////////////////////////
+//// function
+// function printHello(name){
+//     console.log("Hello "+name);
+// }
+
+// printHello("Microsoft");
+// //箭头函数
+// const add = (a,b) => a+b;//隐式返回语式，不用return就能使其返回
+// console.log(add(1,2));
+
+
